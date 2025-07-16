3dc2723480cc98e2e8eb930b27df20b567ccc721
rainingcodes <rainingcodes0@gmail.com>
2025-07-16 12:51:20 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+df = pd.DataFrame(
+    [
+       {"command": "st.selectbox", "rating": 4, "is_widget": True},
+       {"command": "st.balloons", "rating": 5, "is_widget": False},
+       {"command": "st.time_input", "rating": 3, "is_widget": True},
+   ]
+)
+
+# Plotly Bar Chart
+fig = px.bar(
+    df,
+    x="command",     # x축에 command
+    y="rating",      # y축에 rating
+    color="command", # command별 색깔 구분
+    labels={"command": "x축 제목", "rating": "Rating"},
+    title="Command Rating Bar Chart"
+)
+st.plotly_chart(fig, use_container_width=True)
+st.button('Demo')
+st.data_editor('Edit DF', df)
+st.checkbox('Option 1')
+st.radio('Pick Country:', ['France','Germany'])
+st.selectbox('Select', [1,2,3])
+st.multiselect('Multiselect', [1,2,3])
+st.slider('Slide me', min_value=0, max_value=10)
+st.select_slider('Slide to select', options=[1,'2'])
+st.text_input('Enter Article')
+st.number_input('Enter required number')
+st.text_area('Entered article text')
+st.date_input('Select date')
+st.time_input('Select Time')
+st.file_uploader('File CSV uploader')
+# st.download_button('Download Source data', data)
+# st.camera_input('Click a Snap')
+# st.color_picker('Pick a color')
+
+
+ani_list = ['짱구는 못말려', '몬스터', '릭앤모티']
+ani_list = ['짱구는못말려', '몬스터','릭앤모티']
+img_list = ['https://i.imgur.com/t2ewhfH.png', 
+            'https://i.imgur.com/ECROFMC.png', 
+            'https://i.imgur.com/MDKQoDc.jpg']
+
+st.text('Tushar-Aggarwal.com')
+st.markdown('[Tushar-Aggarwal.com](https://tushar-aggarwal.com)')
+st.caption('Success')
+st.latex(r''' e^{i\pi} + 1 = 0 ''')
+st.write('Supreme Applcations by Tushar Aggarwal')
+st.write(['st', 'is <', 3]) # see *
+st.title('Streamlit Magic Cheat Sheets')
+st.header('Developed by Tushar Aggarwal')
+st.subheader('visit tushar-aggarwal.com')
+st.code('for i in range(8): print(i)')
+
+# * optional kwarg unsafe_allow_html = True
+
+
+st.image(img_list[-1])
